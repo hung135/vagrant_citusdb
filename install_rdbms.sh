c4c7c29c3e5c1670802211500ca8fe7fa967dc8b
Hung Nguyen <hung.nguyen@cfpb.gov>
2017-04-05 14:49:06 -0400

    update install shell script

--- install_rdbms.sh
+++ install_rdbms.sh
@@ -1,11 +1,16 @@
 #this will be last and most of the time we won't need and requires 4gb of ram to run
-      docker run  --restart=always --name postgres -e POSTGRES_PASSWORD=ubuntu -p 54322:5432 -d postgres:latest   
+docker build -t postgrescitus .
+      #docker run  --restart=always --name postgres -e POSTGRES_PASSWORD=ubuntu -p 54322:5432 -d postgres:latest   
       # Add Citus repository for package manager
       #https://docs.citusdata.com/en/v6.1/installation/production_deb.html
-      curl https://install.citusdata.com/community/deb.sh | sudo bash
+      #curl https://install.citusdata.com/community/deb.sh | sudo bash
       # install the server and initialize db
-      sudo apt-get -y install postgresql-9.6-citus-6.1
+      #sudo apt-get -y install postgresql-9.6-citus-6.1
 
       # preload citus extension
-      sudo pg_conftool 9.6 main set shared_preload_libraries citus
+      #sudo pg_conftool 9.6 main set shared_preload_libraries citus
+      docker run -d -p 5432:5432 postgrescitus
+      docker run -d -p 5433:5432 postgrescitus
+      docker run -d -p 5434:5432 postgrescitus
+
 
